f2d6b3c9b5d60e68ab99adc10589332b46523db6
Matt Ruston <matt@rustonmail.com>
2017-11-19 13:13:45 -0600

    removed extra files

--- XcodeBuild/Particles.build/Debug/ALL_BUILD.build/Script-19A5A65952494C4BB725215E.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/Matt/Desktop/Graphics/ThatsANiceRenderingDave/XcodeBuild -f /Users/Matt/Desktop/Graphics/ThatsANiceRenderingDave/XcodeBuild/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- XcodeBuild/Particles.build/Debug/ZERO_CHECK.build/Script-21D3F44F1981467CA93C360D.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/Matt/Desktop/Graphics/ThatsANiceRenderingDave/XcodeBuild -f /Users/Matt/Desktop/Graphics/ThatsANiceRenderingDave/XcodeBuild/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
