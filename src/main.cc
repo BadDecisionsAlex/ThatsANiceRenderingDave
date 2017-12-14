e686efb230a289b63bbd69f0787b0d6281ddd0eb
jcristol <joshcristol@gmail.com>
2017-12-14 16:52:02 -0600

    getting close

--- src/main.cc
+++ src/main.cc
@@ -32,34 +32,42 @@ using glm::uvec1;
 
 int window_width = 640, window_height = 640;
 
+void stdSetup(ParticleSystem& p){
+    p.width = window_width;
+    p.height = window_height;
+    p.setup();
+    p.prepareDraw();
+}
+
 int main(int argc, char* argv[])
 {
     OpenGLUtil openGL = OpenGLUtil(window_width, window_height, "Particles");
     GLFWwindow* window = openGL.setup();
     GUI gui(window);
+    vector<ParticleSystem> systems;
     
     // Params in Order : 
     // (int) Number of Cells (N+2 x N+2) because of border
     // (float) dt Timestep (1.0f/60.0f) matches 60 fps
     // (float) diffusion (0.0f-1.0f) density spread rate.
     // (float) viscocity (0.0f-1.0f) velocity spread rate.
-    FluidSystem* rootSystem = new FluidSystem( 
+    FluidSystem rootSystem(
             64,     // N size 
             (1.0f/60.0f), // don't touch
             0.0013f,    // diffusion 
             0.18f     // viscocity
             );
 
-    rootSystem->width = window_width;
-    rootSystem->height = window_height;
-    rootSystem->setup();
-    rootSystem->prepareDraw();
-    gui.delegates.push_back(rootSystem);
+    stdSetup(rootSystem);
+    gui.delegates.push_back(&rootSystem);
+    systems.push_back(rootSystem);
 
 	while (openGL.drawBool()) {
         openGL.beforeDraw();
-        rootSystem->step();
-        rootSystem->draw();
+        for (int i = 0; i < systems.size(); ++i) {
+           systems[i].step();
+            systems[i].draw();
+        }
         openGL.afterDraw();
 	}
     openGL.destroy();
